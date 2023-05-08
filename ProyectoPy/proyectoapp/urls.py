fb03fe7adc99de96c849a7aeaf1566d273e7031f
Natalia de los Angeles Angarita Buitrago <nbuitrago@asesoftware.com>
2023-05-08 17:53:21 -0500

    correccion

--- ProyectoPy/proyectoapp/urls.py
+++ ProyectoPy/proyectoapp/urls.py
@@ -3,8 +3,9 @@ from proyectoapp import views
 
 urlpatterns = [
    
-    path('', views.inicio, name="Inicio"), #este era nuestro primer view
+    path('', views.inicio, name="Inicio"),
     path('producto', views.producto, name="Producto"),
     path('cliente', views.cliente, name="Cliente"),
     path('vendedor', views.vendedor, name="Vendedor"),
+    path('productos', views.productos, name="productos"),
 ]
--- ProyectoPy/proyectoapp/views.py
+++ ProyectoPy/proyectoapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from proyectoapp.models import Producto
+from proyectoapp.models import Producto, Cliente, Vendedor
 from django.http import HttpResponse
-
+from proyectoapp.forms import productoFormulario, clienteFormulario, vendedorFormulario
 # Create your views here.
 def inicio(request):
     return render(request, 'proyectoapp/inicio.html')
@@ -11,4 +11,33 @@ def cliente(request):
     return render(request,'proyectoapp/cliente.html')
 def vendedor(request):
     return render(request,'proyectoapp/vendedor.html')
+
+def productos(request):
+    if request.method == 'post':
+        miFormulario = productoFormulario(request.post)
+        print(miFormulario)
+
+        if miFormulario.is_valid:
+            informacion = miFormulario.cleaned_data
+            producto = Producto(int(informacion['id']),str(informacion['categoria']),str(informacion['nombre']), int(informacion['precio'])) 
+            producto.save()
+            return render(request, 'proyectoapp/inicio.html')
+    else:
+        miFormulario = productoFormulario()
+    return render(request,'proyectoapp/ProductoFormulario.html', {"miFormulario": miFormulario})
+
+
+def cliente(request):
+    if request.method == 'post':
+        miFormulario = clienteFormulario(request.post)
+        print(miFormulario)
+        
+        if miFormulario.is_valid:
+            informacion = miFormulario.cleaned_data
+            producto = Producto(int(informacion['id']),str(informacion['categoria']),str(informacion['nombre']), int(informacion['precio'])) 
+            producto.save()
+            return render(request, 'proyectoapp/inicio.html')
+    else:
+        miFormulario = productoFormulario()
+    return render(request,'proyectoapp/ProductoFormulario.html', {"miFormulario": miFormulario})
 
