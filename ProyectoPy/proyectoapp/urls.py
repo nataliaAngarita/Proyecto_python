547e646938e48fccbafca73e7872cca5466292c6
Natalia de los Angeles Angarita Buitrago <nbuitrago@asesoftware.com>
2023-05-21 01:54:11 -0500

    Entrega final

--- ProyectoPy/proyectoapp/urls.py
+++ ProyectoPy/proyectoapp/urls.py
@@ -10,6 +10,14 @@ urlpatterns = [
     path('productos', views.productos, name="productos"),
     path('buscar/',views.buscar),
     path('leerVendedor', views.leerVendedor, name = "leerVendedor"),
-    path('eliminarVendedor/<vendedor_nombre>/', views.eliminarVendedor, name="EliminarVendedor")
+    path('eliminarVendedor/<vendedor_nombre>/', views.eliminarVendedor, name="EliminarVendedor"),
+    path('editarVendedor/<vendedor_nombre>/', views.editarVendedor, name="EditarVendedor"),
+
+    path('producto/list',views.ProductoList.as_view(),name='List'),
+    path(r'^(?P<pk>\d+)$', views.ProductoDetalle.as_view(),name='Detail'),
+    path(r'^nuevo$', views.ProductoCreacion.as_view(),name='New'),
+    path(r'^editar/(?P<pk>\d+)$',views.ProductoUpdate.as_view(),name='Edit'),
+    path(r'^borrar/(?P<pk>\d+)$',views.ProductoDelete.as_view(),name='Delete')
+
 
 ]
